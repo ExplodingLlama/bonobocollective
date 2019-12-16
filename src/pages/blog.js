4b951acbe7cf07fbaa97bd7466b3ebc73c120e2d
Ratandeep Singh <ratan14@gmail.com>
2019-12-16 16:11:34 +0530

    removed warning

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import blogStyles from "./blog.module.scss"
 import moment from "moment"
